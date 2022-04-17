1d5e2f7ecd08667b4d54c1f141b20b7701d09833
Astronotch <62258837+Astronotch@users.noreply.github.com>
2022-04-17 14:53:03 +0200

    Add files via upload

--- SyntheseVocal/decoupe.py
+++ SyntheseVocal/decoupe.py
@@ -20,19 +20,84 @@ def decoupage(TempDico):
 def decoupePhrase(txt, TempDico):
     phrase = []
     mot = ""
-    phrase2 = []
-    mot2 = ""
     for i in txt:
         a = 0
         while i.lower() != TempDico[a][0]:
             a = a+1
 
-        if TempDico[a][1] != "P":
-            mot = mot + TempDico[a][0]
-            mot2 = mot2 + TempDico[a][1]
-
-        else:
+        if TempDico[a][1] == "P":
             phrase.append(mot)
-            phrase2.append(mot2)
-            mot, mot2 = "", ""
-    return phrase, phrase2
+            mot = ""
+        else:
+            mot = mot + TempDico[a][1]
+    return phrase
+
+def listeP(phraseDecoupe):
+    PolyCombi = []
+    Temp1PolyCombi = ""
+    Temp2PolyCombi = []
+    mot = 0
+    for lettre in phraseDecoupe:
+        for lettre in phraseDecoupe[mot]:
+            Temp1PolyCombi = Temp1PolyCombi + lettre
+            Temp2PolyCombi.append(Temp1PolyCombi)
+        PolyCombi.append(Temp2PolyCombi)
+        Temp1PolyCombi = ""
+        Temp2PolyCombi = []
+        mot = mot+1
+    return PolyCombi
+
+def supprimerModele(CombNom, cpt):
+    #passer le mot en une liste puis supprimer les élements correspondants
+    liste2 = []
+    for i in range(cpt+1):
+        CombNom.pop(0)
+    for a in range(len(CombNom)):
+        liste1 = []
+        for b in range(len(CombNom[a])):
+            liste1.append(CombNom[a][b])
+        liste2.append(liste1)
+    for c in range(cpt+1):
+        if c < len(CombNom):
+            for d in range(len(CombNom)):
+                liste2[d].pop()
+    CombNom = []
+    for i in range(len(liste2)):
+        mot = ""
+        for j in range(len(liste2[i])):
+            mot = mot + liste2[i][j]
+        CombNom.append(mot)
+    return CombNom
+
+def supprimerTexte(texte, cpt):
+    #Passer texte dans une liste avec chaque lettre
+    #Enlever les cpt premiers éléments avec une boucle for
+    #Remettre tout les éléments de la liste en place puis les remettre dans une liste
+    liste1 = []
+    for i in range(cpt+1):
+        texte.pop(0)
+    for a in range(len(texte)):
+        liste2 = []
+        for b in range(len(texte[a])):
+            liste2.append(texte[a][b])
+        liste1.append(liste2)
+    texte = []
+    for c in range(len(liste1)):
+        mot = ""
+        for d in range(cpt+1):
+            liste1[c].pop(0)
+        for e in range(len(liste1[c])):
+            mot = mot + liste1[c][e]
+        texte.append(mot)
+    return texte
+
+def DecoupeMot(texte):
+    liste1 = []
+    lettre = ""
+    for i in texte:
+        if i == " ":
+            liste1.append(lettre)
+            lettre = ""
+        else:
+            lettre = lettre + i
+    return liste1
--- SyntheseVocal/projetSyntheseVocal.py
+++ SyntheseVocal/projetSyntheseVocal.py
@@ -1,80 +1,8 @@
 from decoupe import *
 
-def listeP(phraseDecoupe):
-    PolyCombi = []
-    Temp1PolyCombi = ""
-    Temp2PolyCombi = []
-    mot = 0
-    for lettre in phraseDecoupe:
-        for lettre in phraseDecoupe[mot]:
-            Temp1PolyCombi = Temp1PolyCombi + lettre
-            Temp2PolyCombi.append(Temp1PolyCombi)
-        PolyCombi.append(Temp2PolyCombi)
-        Temp1PolyCombi = ""
-        Temp2PolyCombi = []
-        mot = mot+1
-    return PolyCombi
-
-def supprimerModele(CombNom, cpt):
-    #passer le mot en une liste puis supprimer les élements correspondants
-    liste2 = []
-    for i in range(cpt+1):
-        CombNom.pop(0)
-    for a in range(len(CombNom)):
-        liste1 = []
-        for b in range(len(CombNom[a])):
-            liste1.append(CombNom[a][b])
-        print(liste1)
-        liste2.append(liste1)
-    for c in range(cpt+1):
-        if c < len(CombNom):
-            for d in range(len(CombNom)):
-                liste2[c].pop()
-    CombNom = []
-    for i in range(len(liste2)):
-        mot = ""
-        for j in range(len(liste2[i])):
-            mot = mot + liste2[i][j]
-        CombNom.append(mot)
-    return CombNom
-
-def supprimerTexte(texte, cpt):
-    #Passer texte dans une liste avec chaque lettre
-    #Enlever les cpt premiers éléments avec une boucle for
-    #Remettre tout les éléments de la liste en place puis les remettre dans une liste
-    liste1 = []
-    for i in range(cpt+1):
-        texte.pop(0)
-    for a in range(len(texte)):
-        liste2 = []
-        for b in range(len(texte[a])):
-            liste2.append(texte[a][b])
-        liste1.append(liste2)
-    texte = []
-    for c in range(len(liste1)):
-        mot = ""
-        for d in range(cpt+1):
-            liste1[c].pop(0)
-        for e in range(len(liste1[c])):
-            mot = mot + liste1[c][e]
-        texte.append(mot)
-    return texte
-
-def DecoupeMot(texte):
-    liste1 = []
-    lettre = ""
-    for i in texte:
-        if i == " ":
-            liste1.append(lettre)
-            lettre = ""
-        else:
-            lettre = lettre + i
-    return liste1
-
-
 lettres = open("lettre.txt", "r")
 TempDico = lettres.readlines()
-texte = "essai"
+texte = input("Entrez le mot que vous voulez découper en syllabe :\n")
 texte = texte + " "
 texteD = DecoupeMot(texte)
 
@@ -89,12 +17,12 @@ phraseDecoupe = decoupePhrase(texte, TempDico)
 PolyCombi = listeP(phraseDecoupe)
 texteDecoupPoly = listeP(texteD)
 
+
 z = len(Temp)-1
 a = 0
 b = len(PolyCombi[a])-1
 SyllabeMot1 = []
 SyllabeMot2 = []
-
 
 while a < len(PolyCombi):
     b = len(PolyCombi[a])-1  
@@ -104,7 +32,7 @@ while a < len(PolyCombi):
                 SyllabeMot1.append(Temp[z])
                 SyllabeMot2.append(texteDecoupPoly[a][b])
                 texteDecoupPoly[a] = supprimerTexte(texteDecoupPoly[a], b)
-                PolyCombi[a] = supprimerModele(PolyCombi[a], b) #Le problème est le fait que Polycombi[a] devient une liste de liste, peut-être essayer de faire un .append dans la fonction supprimer et d'agir directement sur PolyCombi[a]
+                PolyCombi[a] = supprimerModele(PolyCombi[a], b)                 
                 if len(PolyCombi[a]) == 0:
                     a = a+1
                 if a < len(PolyCombi):
@@ -118,5 +46,3 @@ while a < len(PolyCombi):
         z = len(Temp)-1
     a = a+1
 print("Fini: ", SyllabeMot2)
-
-#Il faudrait ajouter une fonction qui prend la syllabe qui a été découpé et la stock dans une liste
